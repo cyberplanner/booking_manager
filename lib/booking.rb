2c4e5384827c048e7046bc828e9b23a798eaf3d8
cyberplanner <saleh.gfx@gmail.com>
2016-11-21 01:22:30 +0000

    Second test passed: return time of appointment.

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,9 +1,16 @@
 class Booking
+  OPENING_TIME = "08:00:00"
+  CLOSING_TIME = "15:00:00"
   attr_reader :available_slots
 
   def initialize(available_slots)
     @available_slots = available_slots
   end
 
-  
+  def book_appointment(time)
+    @booked_appointment = @available_slots.find { |slot| slot["time"] >= time }
+    @booked_appointment["time"]
+  end
+
+
 end
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -9,10 +9,20 @@ describe Booking do
   let(:booking_slots) { slots_hash["availability_slots"] }
   let(:booking) { described_class.new(booking_slots) }
 
-  it "stores available slots" do
-    expect(booking.available_slots).to include({"time"=>"08:00:00",
-                                                "slot_size"=>10,
-                                                "doctor_id"=>1})
+  describe "Initialized with available time slots" do
+    it "Stores available slots" do
+      expect(booking.available_slots).to include({"time"=>"08:00:00",
+                                                  "slot_size"=>10,
+                                                  "doctor_id"=>1})
+    end
+  end
+
+  describe "Booking appointments" do
+    context "When time slot is avaiable" do
+      it 'returns the time for the next available appointment' do
+        expect(booking.book_appointment("08:00:00")).to eq "08:00:00"
+      end
+    end
   end
 
 
