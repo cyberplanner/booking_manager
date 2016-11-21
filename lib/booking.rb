6237011db599ee8ac2c0794e89cab0b35975e4d4
cyberplanner <saleh.gfx@gmail.com>
2016-11-21 03:36:33 +0000

    WIP: testing errors with Tracis CI

--- lib/booking.rb
+++ lib/booking.rb
@@ -10,6 +10,8 @@ class Booking
   end
 
   def book_appointment(time)
+    raise "Sorry, you can't book an appointment before work hours" if time < OPENING_TIME
+    raise "Sorry, you can't book an appointment after work hours" if time > CLOSING_TIME
     @booked_appointment = @available_slots.find { |slot| slot["time"] >= time }
     raise "Sorry, there's no more avaiable appointments today" if @booked_appointment.nil?
     @booked_slots << @available_slots.delete(@booked_appointment)
