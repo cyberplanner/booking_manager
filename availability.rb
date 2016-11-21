dfaf19365e9523acb5ebe6381da0fd548fbd113d
cyberplanner <saleh.gfx@gmail.com>
2016-11-21 05:25:07 +0000

    :lipstick: cosmetic changes.

--- availability.rb
+++ availability.rb
@@ -11,12 +11,15 @@ booking = Booking.new(available_slots)
 input_time = ARGV[0]
 
 if input_time.nil?
+  puts "------------"
   puts Rainbow("ERROR:").underline.bright + Rainbow(" Appointment's time can not be left empty, please enter appointment's after ").red + Rainbow("'./availability.rb'").yellow + Rainbow("!").red
+  puts "------------"
 else
   formated_time = input_time + ":00"
   booking_time = booking.book_appointment(formated_time)
-
-  puts Rainbow("Your appointment's time is: ").underline.bright + Rainbow("#{booking_time}").underline.bg(:green)
+  puts "------------"
+  puts Rainbow("Your appointment's time is:").underline.bright + Rainbow(" #{booking_time[0..-4]}").underline.blink.green
   puts ""
   puts Rainbow("Have a nice day!").red
+  puts "------------"
 end
