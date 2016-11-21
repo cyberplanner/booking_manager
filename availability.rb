0b851ed3c5f5af84bb9560c539dfe0199c797724
cyberplanner <saleh.gfx@gmail.com>
2016-11-21 05:19:15 +0000

    :tada: Adds ./availability.rb, adds rainbow.

---
+++ availability.rb
@@ -0,0 +1,22 @@
+#! /usr/bin/env ruby
+require './lib/booking'
+require 'json'
+require 'rainbow'
+
+file = File.read('availability_slots.json')
+slots_hash = JSON.parse(file)
+available_slots = slots_hash["availability_slots"]
+
+booking = Booking.new(available_slots)
+input_time = ARGV[0]
+
+if input_time.nil?
+  puts Rainbow("ERROR:").underline.bright + Rainbow(" Appointment's time can not be left empty, please enter appointment's after ").red + Rainbow("'./availability.rb'").yellow + Rainbow("!").red
+else
+  formated_time = input_time + ":00"
+  booking_time = booking.book_appointment(formated_time)
+
+  puts Rainbow("Your appointment's time is: ").underline.bright + Rainbow("#{booking_time}").underline.bg(:green)
+  puts ""
+  puts Rainbow("Have a nice day!").red
+end
