a5a93112ce2759068e3221ef3c30b2b6fff2263c
cyberplanner <saleh.gfx@gmail.com>
2016-11-21 00:00:45 +0000

    First test passed: stores slots.

---
+++ lib/booking.rb
@@ -0,0 +1,9 @@
+class Booking
+  attr_reader :available_slots
+
+  def initialize(available_slots)
+    @available_slots = available_slots
+  end
+
+  
+end
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -1,3 +1,19 @@
+require 'booking'
+require 'json'
+
+file = File.read('availability_slots.json')
+availability_hash = JSON.parse(file)
+availability_slots = availability_hash["availability_slots"]
+
 describe Booking do
-  
+  let(:booking) { described_class.new(availability_slots) }
+  let(:availability_slots) { availability_slots }
+
+  it "stores available slots" do
+    expect(booking.available_slots).to include({"time"=>"08:00:00",
+                                                "slot_size"=>10,
+                                                "doctor_id"=>1})
+  end
+
+
 end
